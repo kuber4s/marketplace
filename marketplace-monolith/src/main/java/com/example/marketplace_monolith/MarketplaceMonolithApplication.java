a2a0f78ea18288cda3b837f77d3e53459034476c
kuber4s <cheguevarych@gmail.com>
2025-08-30 19:32:54 +0300

    Add files via upload

---
+++ marketplace-monolith/src/main/java/com/example/marketplace_monolith/MarketplaceMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplace_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceMonolithApplication.class, args);
+	}
+
+}
---
+++ marketplace-monolith/src/test/java/com/example/marketplace_monolith/MarketplaceMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketplace_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
